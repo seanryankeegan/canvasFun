0ca9ef44a2c5ed359aebca0a7653b3bae7b70d23
Sean Keegan <seanryankeegan@gmail.com>
2016-09-25 20:55:31 -0400

    write hi in canvas

--- main.js
+++ main.js
@@ -7,8 +7,38 @@ window.onload = function(){
 
   console.log("canvas is ", canvas);
   console.log("c is ", c);
-  c.fillStyle = "blue";
-  c.fillRect(0, 0, canvas.width, canvas.height)
+  c.fillStyle = "red";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+
+  c.fillStyle= "blue";
+  c.fillRect(20,20,360, 360);
+
+  c.strokeStyle = "pink";
+  c.lineWidth = 10;
+
+  c.beginPath();
+  c.moveTo(50, 100);
+  c.lineTo(50, 200);
+  c.lineTo(50, 150);
+  c.lineTo(100, 150);
+  c.lineTo(100, 100);
+  c.lineTo(100, 200);
+  c.stroke();
+
+  c.strokeStyle = "yellow";
+  c.lineWidth = 10;
+
+  c.beginPath();
+  c.moveTo(125, 105);
+  c.lineTo(175, 105);
+  c.lineTo(150, 105);
+  c.lineTo(150, 195);
+  c.lineTo(125, 195);
+  c.lineTo(175, 195);
+  c.stroke();
+
+
+
 
 })
 
